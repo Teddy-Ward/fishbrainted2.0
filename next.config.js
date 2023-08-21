4b630026742dbeff4ff5745716ec50b9d530baa1
Ted Ward <teddyward@gmail.com>
2023-08-21 16:31:22 +0100

    config change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  supabase: {
+    client: {
+        auth: {
+            persistSession: true //or true
+        }
+    }
+  }
+}
 
 module.exports = nextConfig
